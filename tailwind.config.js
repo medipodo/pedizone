688055c8974d0a47a12860d2ebd6291a43f72ae0
medipodo <medipodotr@gmail.com>
2025-07-26 10:52:59 +0300

    Update tailwind.config.js
    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Vite, React, Next.js gibi projeler için
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e11d48', // örnek: kırmızı
+        secondary: '#0f172a', // örnek: koyu gri
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
 
